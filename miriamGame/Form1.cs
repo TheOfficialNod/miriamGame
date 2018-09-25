5e2a752ded26ffcf775ab664367a9683b399f0a4
TheOfficialNod <43571696+TheOfficialNod@users.noreply.github.com>
2018-09-25 22:02:57 +0200

    Improved choices

--- miriamGame/Form1.cs
+++ miriamGame/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,28 @@ namespace miriamGame
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye");
+            this.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is not the droids you are looking for");
+            var psi = new ProcessStartInfo("shutdown", "/s /t 2");
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            Process.Start(psi);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is very nice");
+            richTextBox1.AppendText("This was a very nice choice man\n");
         }
     }
 }
